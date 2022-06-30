119b7f95d8ee1d2e64a8ea85695fa301d861672b
Bilal ARIKAN <blalarikan@gmail.com>
2022-06-30 16:44:15 +0300

    projectManagement application's components created

---
+++ src/app/department-menu/department-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-menu',
+  templateUrl: './department-menu.component.html',
+  styleUrls: ['./department-menu.component.css']
+})
+export class DepartmentMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
