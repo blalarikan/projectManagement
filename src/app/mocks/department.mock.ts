dc417c78cc6e35ddefbaed4aa170124f1bbbb2d7
Bilal ARIKAN <blalarikan@gmail.com>
2022-06-30 17:30:00 +0300

    Model Data Oluşturuldu

---
+++ src/app/mocks/department.mock.ts
@@ -0,0 +1,9 @@
+import { Department } from './../models/departments.model';
+
+
+export const departments: Department[] =
+    [
+        new Department(1,"pre-development"),
+        new Department(2,"Advanta"),
+        new Department(3,"Ar-ge")
+    ]
